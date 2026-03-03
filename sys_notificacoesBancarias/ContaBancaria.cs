a76c0a94977e6514c15be8a7a55b698af1cb8a01
wcc_devs <wcc_devs@WCC-LAB36-61325>
2026-03-03 19:03:43 -0300

    Add project files.

---
+++ sys_notificacoesBancarias/ContaBancaria.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sys_notificacoesBancarias
+{
+    internal class ContaBancaria
+    {
+        private string _titular;
+        private double _saldo;
+        private double _limiteExtra;
+
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+        public double LimiteExtra{
+            get;
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("O limite extra não pode ser negativo.");
+                }
+            }
+            
+        }
+
+        public ContaBancaria(string titular, double saldo, double limiteExtra)
+        {
+            titular = Titular;
+
+            saldo = Saldo;
+
+            limiteExtra = LimiteExtra;
+                ;
+
+        }
+    }
+}
---
+++ sys_notificacoesBancarias/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace sys_notificacoesBancarias
+{
+    internal class Program
+    {
+        public delegate void NotificacaoAlerta(string mensagem);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
